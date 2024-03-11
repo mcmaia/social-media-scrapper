5603550c27a6e81607d1d3667766706119019d15
mcmaia <marcelo.castromaia@gmail.com>
2024-03-11 17:38:29 -0300

    feat: added dato to bq

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from posts import ig_posts_to_sql
+from posts import ig_posts_to_sql, post_to_bq
 from profiles import ig_profiles_to_sql
+import pandas as pd
+from database import Base, SessionLocal, engine
+from models import InstagramPost
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("MY_APIFY_TOKEN")
-DATASET = os.getenv("APIFY_DATASET")
+
+# Your environment variables
+MY_APIFY_TOKEN = os.getenv("MY_APIFY_TOKEN")
+APIFY_DATASET = os.getenv("APIFY_DATASET")
+BQ_DATA_SET = os.getenv("BQ_DATA_SET")
+BQ_DATA_SET_LOCATION = os.getenv("BQ_DATA_SET_LOCATION")
+BQ_TABLE_ID = os.getenv("BQ_TABLE_ID")
 PROFILE_DATASET = os.getenv("APIFY_PROFILE_DATASET")
 
-ig_posts_to_sql(DATASET, API_KEY)
-ig_profiles_to_sql(PROFILE_DATASET, API_KEY)
+# Set up the session
+Base.metadata.create_all(bind=engine)
+session = SessionLocal()
+
+# Querying InstagramPost
+query_result = session.query(InstagramPost).all()
+
+# Convert query results to a list of dictionaries
+data = [{key: getattr(post, key) for key in post.__dict__.keys() if not key.startswith('_')} for post in query_result]
+
+# Convert the list of dictionaries to a DataFrame
+psql_table_df = pd.DataFrame(data)
+psql_table_df = psql_table_df.astype(str)
+
+# Call functions
+ig_posts_to_sql(APIFY_DATASET, MY_APIFY_TOKEN)
+post_to_bq(psql_table_df, BQ_DATA_SET, BQ_TABLE_ID, BQ_DATA_SET_LOCATION)
+
+ig_profiles_to_sql(PROFILE_DATASET, MY_APIFY_TOKEN)
--- posts.py
+++ posts.py
@@ -1,17 +1,19 @@
 import json
 import pandas as pd
-from database import engine
-from models import Base
-from competitors_mapping import client_1, client_2, client_3, client_4, client_qui, client_tai, client_tar, client_ypy
-
 import requests
+from google.cloud import bigquery
+from google.oauth2.service_account import Credentials
+import pandas_gbq
+
+from database import engine
+
 
 def ig_posts_to_sql(apify_dataset, apify_key):
 
     oauth_url = f"https://api.apify.com/v2/datasets/{apify_dataset}/items?token={apify_key}"
 
-    # Set up the session
-    Base.metadata.create_all(bind=engine)
+    # # Set up the session
+    # Base.metadata.create_all(bind=engine)
     
     res = requests.get(url=oauth_url)
 
@@ -63,21 +65,35 @@ def ig_posts_to_sql(apify_dataset, apify_key):
     }, 
     inplace=True)
 
-    # Drop complex xolumns
-    df = df.drop(columns=['music_info', 'hashtags', 'mentions', 'images', 'child_posts', 'tagged_users', 'coauthor_producers', 'latest_comments', 'first_comment', 'is_pinned'])
-
+    try:
+        # Columns to drop if they exist
+        columns_to_drop = ['music_info', 'hashtags', 'mentions', 'images', 'child_posts', 'tagged_users', 'coauthor_producers', 'latest_comments', 'first_comment', 'is_pinned']
+        
+        # Drop columns if they exist
+        for col in columns_to_drop:
+            if col in df.columns:
+                df = df.drop(columns=col)
+    except Exception:
+        pass
 
     # Each competitor of our clients
     def determine_client(username):
-        username = str(username)
-        if username in client_ypy:
-            return client_1
-        elif username in client_tar:
-            return client_2
-        elif username in client_qui:
-            return client_3   
-        elif username in client_tai:
-            return client_4   
+        if username in [
+            "institutoupdate", "movimentosemterra", "apiboficial", "estamosprontas", "vamosjuntasorg", "redeaponte", "institutoperegum", "juventudenegrapolitica", "girlupbrasil", "climadeeleicao", "instadatenda", "seja.democracia", "elasnopoderbr", "votenelas", "novasnarrativasevangelicas", "advocacy_hub", "institutoalziras", "todaznapolitica", "raps_brasil", "_nossas", "somos_quid", "laboraoficinaeleitoral", "fogocruzadobr", "votelgbt", "movimentoacredito", "euvotoemnegra", "mulheres_nas_urnas", "mevotoserafeminista", "casafluminense", "anacarolinalouren", "ariovaldo", "biancasantanadelua", "caetanoscannavino", "clementeganz", "professorheliosantos", "marcio.black", "institutomariellefranco", "isernarede", "perifaconnection", "perifa_lab", "coalizaonegrapordireitos", "conectas", "instituto.soudapaz", "fundacaotide", "aguaesaneamento_", "institutoalziras", "iepsoficial", "procomum", "institutotalanoa", "institutounibanco", "pactopelademocracia", "transparenciainternacionalbr"
+        ]:
+            return 'ypykuera'
+        elif username in [
+            "lucasmiglio", "tarcirleidebrito", "comandante_marinho", "bruno.balarini", "delegadojefersonbotelho"
+        ]:
+            return 'tarcirlei'
+        elif username in [
+            "quilomboperiferico", "keit.lima", "jussarabassosp", "luanapsol", "toninhovespoli", "celsogiannazi", "lunazarattini", "deboralimamtst", "ivonesilvasp", "quilomboperiferico"
+        ]:
+            return 'quilombo_periferico'   
+        elif username in [
+            "carlosbolsonaro", "tainadepaularj", "luciananovaesoficial", "edsonsantosrio", "cesarmaia", "sou_thaisferreira"
+        ]:
+            return 'taina_de_paula'         
         else:
             return 'Other'  
 
@@ -89,6 +105,79 @@ def ig_posts_to_sql(apify_dataset, apify_key):
 
     try:
         df.to_sql('instagram_posts_test', engine, if_exists='append', index=False)
-        print("Data loaded successfully: 200")
+        print("Data loaded successfully to psql: 200")
     except Exception as e:
-        print(f"Something went wrong: {e}")
+        print(f"Something went wrong sending data to psql: {e}")
+
+
+def post_to_bq(psql_table, bq_dataset_id, bq_table_id, bq_dataset_location):
+    
+    credentials = Credentials.from_service_account_file('config/gcp_credentials.json')
+    client = bigquery.Client.from_service_account_json('config/gcp_credentials.json')
+    
+
+    # Create a new dataset
+    dataset_id = bq_dataset_id
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = bq_dataset_location
+    try:
+        dataset = client.create_dataset(dataset)
+    except Exception:
+        pass
+
+    table_id = bq_table_id
+    schema = [
+        bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("username", "STRING"),
+        bigquery.SchemaField("id_post", "STRING"),
+        bigquery.SchemaField("input_url", "STRING"),
+        bigquery.SchemaField("type", "STRING"),
+        bigquery.SchemaField("short_code", "STRING"),
+        bigquery.SchemaField("caption", "STRING"),
+        bigquery.SchemaField("url", "STRING"),
+        bigquery.SchemaField("comments_count", "INTEGER"),
+        bigquery.SchemaField("dimensions_height", "INTEGER"),
+        bigquery.SchemaField("dimensions_width", "INTEGER"),
+        bigquery.SchemaField("display_url", "STRING"),
+        bigquery.SchemaField("video_url", "STRING"),
+        bigquery.SchemaField("alt", "STRING"),
+        bigquery.SchemaField("likes_count", "INTEGER"),
+        bigquery.SchemaField("video_view_count", "INTEGER"),
+        bigquery.SchemaField("video_play_count", "INTEGER"),
+        bigquery.SchemaField("timestamp", "TIMESTAMP"),
+        bigquery.SchemaField("location_name", "STRING"),
+        bigquery.SchemaField("location_id", "STRING"),
+        bigquery.SchemaField("owner_full_name", "STRING"),
+        bigquery.SchemaField("owner_username", "STRING"),
+        bigquery.SchemaField("owner_id", "STRING"),
+        bigquery.SchemaField("product_type", "STRING"),
+        bigquery.SchemaField("video_duration", "FLOAT"),
+        bigquery.SchemaField("is_sponsored", "STRING"),
+        bigquery.SchemaField("error", "STRING"),
+        bigquery.SchemaField("client", "STRING"),
+        # For JSON fields, BigQuery supports the STRING type, but you could also use STRUCT or RECORD for more complex structures
+        # bigquery.SchemaField("musicInfo", "STRING"),
+        # bigquery.SchemaField("hashtags", "STRING"),
+        # bigquery.SchemaField("mentions", "STRING"),
+        # bigquery.SchemaField("images", "STRING"),
+        # bigquery.SchemaField("childPosts", "STRING"),
+        # bigquery.SchemaField("taggedUsers", "STRING"),
+        # bigquery.SchemaField("coauthorProducers", "STRING"),
+    ]
+    
+    table_ref = dataset_ref.table(table_id)
+    table = bigquery.Table(table_ref, schema=schema)
+
+    try:
+        pandas_gbq.to_gbq(psql_table,
+                        destination_table=f"{dataset_id}.{table_id}",
+                        project_id='staging-voxis',
+                        if_exists='replace',
+                        credentials=credentials)
+        print("Data loaded successfully to BQ: 200")
+    except Exception as e:
+        print(f"Something went wrong sending data to BQ: {e}")
+    
+       
+
